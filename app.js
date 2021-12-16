d2fd22fceb7f7606dd62dec5aea6059551a29ab9
Victor Caruso <vic.caruso@gmail.com>
2021-12-16 12:40:15 -0800

    login and signup working w/ redirect

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // import functions and grab DOM elements
+import { userLogin, userSignup, pollsRedirect } from './fetch-utils.js';
 
-// let state
+// Login and Signup forms
+const loginFormEl = document.querySelector('#login-form');
+const signupFormEl = document.querySelector('#signup-form');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// Check if a user is already signed in and if so, re-direct to polls page
+
+// Handle Sign Up
+signupFormEl.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(signupFormEl);
+    const email = data.get('email');
+    const pass = data.get('password');
+
+    await userSignup(email, pass);
+
+    pollsRedirect();
+});
+// Handle Login
+loginFormEl.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(loginFormEl);
+    const email = data.get('email');
+    const pass = data.get('password');
+
+    await userLogin(email, pass);
+
+    pollsRedirect();
+});
+

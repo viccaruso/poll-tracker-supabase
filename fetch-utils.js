07ef133a8e37fccf18a511c0dac8006198fda3e6
Victor Caruso <vic.caruso@gmail.com>
2021-12-16 16:43:08 -0800

    set up poll html

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTUwOTAyMSwiZXhwIjoxOTU1MDg1MDIxfQ.v4B-VNkc9Xc9bIM4ig0BrZcgdU2bqx3VGiJiMMYNcis';
 
-const SUPABASE_URL = "https://iddyxpegdpnmmnebvghi.supabase.co";
+const SUPABASE_URL = 'https://iddyxpegdpnmmnebvghi.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
--- polls/polls.js
+++ polls/polls.js
@@ -1,6 +1,18 @@
 import { logout } from '../fetch-utils.js';
+import {  } from '../render-utils.js';
+
 const logoutButtonEl = document.querySelector('#logout-button');
+const currentPollContainerEl = document.querySelector('#current-poll-container');
+const pastPollsContainerEl = document.querySelector('#past-polls-container');
+
+// State
+
+window.addEventListener('load', async() => {
+    
+    
+});
 
 logoutButtonEl.addEventListener('click', async() => {
     await logout();
 });
+

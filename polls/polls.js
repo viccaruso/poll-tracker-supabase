f9c83ba0f7d934fcbd8da0b43eedcc6fdc789bb1
Victor Caruso <vic.caruso@gmail.com>
2021-12-17 14:21:17 -0800

    vote buttons increment vote counters

--- polls/polls.js
+++ polls/polls.js
@@ -2,10 +2,16 @@ import { logout } from '../fetch-utils.js';
 import {  } from '../render-utils.js';
 
 const logoutButtonEl = document.querySelector('#logout-button');
+const optionOneVoteButton = document.querySelector('#vote-one');
+const optionTwoVoteButton = document.querySelector('#vote-two');
+const optionOneVoteCountEl = document.querySelector('#option-one-text');
+const optionTwoVoteCountEl = document.querySelector('#option-two-text');
 const currentPollContainerEl = document.querySelector('#current-poll-container');
 const pastPollsContainerEl = document.querySelector('#past-polls-container');
 
 // State
+let voteOneCount = 0;
+let voteTwoCount = 0;
 
 window.addEventListener('load', async() => {
     
@@ -16,3 +22,12 @@ logoutButtonEl.addEventListener('click', async() => {
     await logout();
 });
 
+optionOneVoteButton.addEventListener('click', () => {
+    voteOneCount++;
+    optionOneVoteCountEl.textContent = voteOneCount;
+});
+
+optionTwoVoteButton.addEventListener('click', () => {
+    voteTwoCount++;
+    optionTwoVoteCountEl.textContent = voteTwoCount;
+});
